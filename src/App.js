365a0f9f0b035eb2bb057785a7b33fc62c503f85
Wayne <wabbruscatoorr@gmail.com>
2020-03-18 20:42:10 -0400

    updating pagination.js and adding bootstrap to index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Axios from "axios";
 
-import Example from "./Components/Carousel";
+import styled from "styled-components";
+import { Jumbotron } from "reactstrap";
+import Card from "./Components/Card";
+import Pagination from "./Components/Pagination";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+  const [data, setData] = useState(false);
+  useEffect(() => {
+    Axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(response => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log("an error", error);
+      });
+  }, []);
 
-      <Example />
-    </div>
+  const Container = styled.section`
+    .jumbotron {
+      background: red;
+    }
+  `;
+
+  return (
+    <Container className="App">
+      <Jumbotron>THIS IS THE JUMBOTRON!!</Jumbotron>
+      {!data && <p>...LoAdInG iN pRoGrEsS!</p>}
+      {data && <Card data={data} />}
+      {<Pagination data={data} />}
+    </Container>
   );
 }
 
